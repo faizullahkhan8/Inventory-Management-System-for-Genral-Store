ab8887e6bd62154ad2a9e8f31a63eb31995b7b1b
Faiz Ullah Khan <faizullahofficial0@gmail.com>
2025-10-29 16:17:55 +0500

    add auth middleware

---
+++ App/server/middlewares/auth.middleware.js
@@ -0,0 +1,7 @@
+export const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    } else {
+        return res.status(401).json({ message: "Unauthorized Access" });
+    }
+};
